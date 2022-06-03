2ccbdd52d1a48a097d5901a77816e0728f5dff01
Alex Thompson <Alex@appNerd.com.au>
2022-06-03 19:12:13 +1000

    Initial Commit

---
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Alexander Thompson on 3/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Alexander Thompson on 3/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
